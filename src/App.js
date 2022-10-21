9287ff45d40342ec8ef4edfb7540046946bf4fbb
Obiora Igboanusi <cwertlinks@gmail.com>
2022-10-21 14:16:56 +0100

    Feat: Add demo contents

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Hello World - Docker</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat vero
+        at deleniti minus? Sapiente reiciendis enim accusantium cum! Recusandae
+        veniam esse, ab, quas perferendis ut minima a porro architecto sunt
+        praesentium voluptates dolores quaerat dolorum nisi quod atque nostrum
+        animi! Consequuntur veritatis, numquam animi a saepe sit explicabo
+        dolore enim corrupti itaque, officia non earum dolores quidem ut harum
+        impedit libero voluptatibus delectus voluptates voluptas nisi labore.
+        Pariatur labore corporis enim dolore, sapiente eligendi facilis nihil?
+        Similique dolorem fuga autem reiciendis quas vero tempore, nesciunt
+        dignissimos enim nam dolorum voluptatem consectetur aperiam cumque. Aut
+        corporis sapiente iure similique illo esse eum, aliquam dolor enim.
+        Tempora itaque fugiat qui commodi hic, ipsa corrupti ad ut temporibus.
+        Libero odio, ipsa maxime aspernatur deleniti quasi delectus, esse
+        inventore alias ipsum numquam dolore obcaecati consequatur dicta
+        distinctio eos odit veritatis itaque commodi repudiandae minus? Tenetur
+        fugiat dolorum ea doloribus velit odit rem laborum ipsam architecto
+        inventore quaerat eum nesciunt vitae, veniam quod aspernatur numquam
+        alias repudiandae temporibus ab omnis assumenda et culpa obcaecati! Id
+        iste architecto, harum laborum possimus obcaecati dolores dignissimos
+        impedit, nam quisquam officia vitae? Omnis reprehenderit, consequuntur
+        dignissimos magnam voluptates mollitia numquam voluptatum. Inventore
+        beatae ut eaque est minus voluptas sequi pariatur repudiandae. Suscipit
+        repudiandae aspernatur corporis quod hic earum inventore exercitationem
+        consequatur quasi, illo unde maxime fuga cumque, nobis velit iste
+        magnam! Rerum, natus. Totam labore dolorum cumque amet quia perspiciatis
+        natus suscipit aliquid necessitatibus! Consectetur beatae quae deserunt
+        id rem a maiores unde officia obcaecati perspiciatis earum maxime,
+        debitis iusto, nihil saepe non, odit vero! Laboriosam odit, eos minima
+        ducimus consequatur quidem adipisci est quis saepe nulla labore porro
+        rerum cupiditate neque ea odio. Sint mollitia nemo aliquam atque
+        suscipit magni odit culpa ratione alias quaerat, illo hic tempora quia
+        voluptatem dolor officia esse sed debitis, nihil facilis? Rerum fuga
+        totam dolorum perferendis et placeat dolorem tempora aliquid possimus
+        qui pariatur iure hic nostrum, dignissimos eum vel odit quis fugiat!
+        Velit alias deleniti reprehenderit eaque recusandae ullam temporibus
+        consequuntur aperiam, fugit veniam nulla ea, perferendis laudantium
+        voluptatem consectetur facilis dignissimos labore necessitatibus rerum
+        saepe vel dolorem. Nesciunt est tempore esse quos possimus perferendis
+        cum maiores sed? Voluptatibus, error. Vitae rerum voluptates deserunt
+        provident dolores blanditiis facere, sit, adipisci cum facilis vel
+        libero consequatur dolorem ea impedit quidem harum labore corrupti
+        distinctio modi recusandae, nihil in quisquam! Maiores animi veritatis
+        architecto tempora qui incidunt, odit tenetur eveniet exercitationem
+        sequi iure ea temporibus aliquid quae molestiae quibusdam ipsa?
+        Accusantium, quam? Quia sapiente culpa, laudantium quasi alias minus
+        magnam voluptas dolorem corporis accusamus non voluptatum! Eos
+        laudantium dolore voluptates quis necessitatibus delectus adipisci
+        excepturi? Reiciendis ipsa cupiditate similique ipsam praesentium. Non
+        tenetur blanditiis consequuntur ipsum fuga suscipit mollitia quam
+        consequatur delectus? Veniam at expedita, in aut numquam iusto illo nisi
+        neque accusamus ipsum ad ut! Expedita, quo dicta libero voluptate sint
+        odit fuga necessitatibus reprehenderit omnis impedit hic? Deserunt
+        placeat vero minus dolores consectetur magni illo! Veritatis unde ex
+        nesciunt rerum quo facere quam odio, optio non iste quod ab aliquid
+        aspernatur. Quam!
+      </p>
     </div>
   );
 }
